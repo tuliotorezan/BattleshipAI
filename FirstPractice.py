d096d920f93497f8a24d11a44e04d5af07360fbf
Tulio Torezan <tuliotorezan@gmail.com>
2022-10-24 09:43:05 -0300

    Learning how to setup a gym environment for a simple game

---
+++ FirstPractice.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+##Importing libraries
+import numpy as np
+from gym import Env
+from gym.spaces import Box, Discrete
+import random
+
+
+##Creating custom reinforced learning environment
+class CustomEnv(Env):
+    def __init__(self):
+        self.action_space = Discrete(3)  #for battleship will probably make this a set of 2 coordinates, each, from 0-10
+        self.observation_space = Box(low=np.array([0]), high=np.array([100])) #will probably hold the battlefield here
+        self.state = 38 + random.randint(-3, 3)
+        self.shower_length = 60  #for battleship this will probably hold the max ammount of turns, in case no one wins
+        
+        
+    #the step function defines what should be done after taking an action in our game
+    def step(self, action):
+        self.state = self.state + action -1
+        self.shower_length -= 1
+        
+        #Calculating reward
+        if self.state >= 37 and self.state <=39:
+            reward = 1
+        else:
+            reward = -1
+            
+        #Checking if shower is done
+        if self.shower_length <=0:
+            done = True
+        else:
+            done = False
+            
+        #Setting placeholder for info
+        info = {}
+        
+        return self.state, reward, done, info
+    
+    def render(self):
+        #this is where you should write the visualiozation code
+        return 0
+        
+    def reset(self):
+        self.state = 38 + random.randint(-3, 3)
+        self.shower_length = 60
+        return self.state
+    
+    
+env = CustomEnv()
+
+episodes = 20
+for episode in range(1, episodes+1):
+    state = env.reset()
+    done = False
+    score = 0
+    
+    while not done:
+        action = env.action_space.sample()
+        n_state, reward, done, info = env.step(action)
+        score += reward
+    print("Episode:{} Score:{}".format(episode, score))
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
